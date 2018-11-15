96e251b72e8f124bf2e2b2cfe70d9558c5166571
L-Lawwliet <794331482@qq.com>
2018-11-16 01:12:13 +0800

    11-15\

--- src/views/modules/order.vue
+++ src/views/modules/order.vue
@@ -8,12 +8,12 @@
         <el-row class="order-item-wrapper" v-for="item in goods" :key="item.id">
           <el-row class="header">
             <el-col :span="12" class="order-num">
-              <span>订单号：{{item.ORDER_NO}}</span>
+              <span>订单ID：{{item.PRODUCT_ID}}</span>
             </el-col>
             <el-col :span="12" class="primary">
               <span v-if="item.STATUS==='0'">未发布</span>
               <span v-if="item.STATUS==='1'">已发布</span>
-              <span v-if="item.STATUS==='2'">已完成</span>
+              <span v-if="item.STATUS==='3'">已完成</span>
             </el-col>
           </el-row>
           <el-row class="name">{{item.PRODUCT_NAME}}</el-row>
@@ -38,139 +38,153 @@
 </template>
 
 <script>
-import api from './../../api/api.js';
-import { XHeader, Loading } from "vux";
-import scroll from './../../components/scroll';
-export default {
-  data() {
-    return {
-      goods: [],
-      pageSize: 20,
-      pageIndex: 1,
-      loadText: '',
-      loading: false,
+    import api from './../../api/api.js';
+    import {
+        XHeader,
+        Loading
+    } from "vux";
+    import scroll from './../../components/scroll';
+    export default {
+        data() {
+            return {
+                goods: [],
+                pageSize: 20,
+                pageIndex: 1,
+                loadText: '',
+                loading: false,
 
-    };
-  },
-  mounted() {
-    this.getMyOrders();
-  },
-  methods: {
-    getMyOrders(callback) {
-      let params = {
-        showCount: this.pageSize,
-        currentPage: this.pageIndex
-      };
-      this.loading = true;
-      api.getMyOrders(params).then(res => {
-        this.loading = false;
-        this.loadText = '';
-        if (callback) callback();
-        if (res.data.msg_code === 'success') {
-          if (this.pageIndex === 1) {
-            this.goods = res.data.result;
-          } else {
-            this.goods = this.goods.concat(res.data.result);
-          }
-        } else {
-          this.$message({
-            type: 'info',
-            message: res.data.msg
-          })
-          this.goods = [];
+            };
+        },
+        mounted() {
+            this.getMyOrders();
+        },
+        methods: {
+            getMyOrders(callback) {
+                let params = {
+                    showCount: this.pageSize,
+                    currentPage: this.pageIndex
+                };
+                this.loading = true;
+                api.getMyOrders(params).then(res => {
+                    this.loading = false;
+                    this.loadText = '';
+                    if (callback) callback();
+                    if (res.data.msg_code === 'success') {
+                        if (this.pageIndex === 1) {
+                            this.goods = res.data.result;
+                        } else {
+                            this.goods = this.goods.concat(res.data.result);
+                        }
+                    } else {
+                        this.$message({
+                            type: 'info',
+                            message: res.data.msg
+                        })
+                        this.goods = [];
+                    }
+                })
+            },
+            fmtDate(obj) {
+                var date = new Date(obj);
+                var y = 1900 + date.getYear();
+                var m = "0" + (date.getMonth() + 1);
+                var d = "0" + date.getDate();
+                return y + "-" + m.substring(m.length - 2, m.length) + "-" + d.substring(d.length - 2, d.length);
+            },
+            onRefresh(callback) {
+                this.getMyOrders(callback);
+            },
+            onInfinite(callback) {
+                this.loadText = '加载中';
+                this.pageIndex++;
+                this.getMyOrders(callback);
+            }
+        },
+        components: {
+            XHeader,
+            Loading,
+            scroll
         }
-      })
-    },
-    fmtDate(obj) {
-      var date = new Date(obj);
-      var y = 1900 + date.getYear();
-      var m = "0" + (date.getMonth() + 1);
-      var d = "0" + date.getDate();
-      return y + "-" + m.substring(m.length - 2, m.length) + "-" + d.substring(d.length - 2, d.length);
-    },
-    onRefresh(callback) {
-      this.getMyOrders(callback);
-    },
-    onInfinite(callback) {
-      this.loadText = '加载中';
-      this.pageIndex++;
-      this.getMyOrders(callback);
-    }
-  },
-  components: {
-    XHeader,
-    Loading,
-    scroll
-  }
-};
+    };
 </script>
 
 <style scoped>
-.order .order-wrapper {
-  padding: 0.6em 0.8em;
-  height: calc(100% - 6.6em);
-  overflow: auto;
-  background: url("./../../assets/img/back.jpg");
-  background-size: 100%;
-  background-repeat: no-repeat;
-}
-.order-item-wrapper {
-  margin-bottom: 1em;
-  padding: 0.5em 1em;
-  background-color: #fff;
-}
-.primary {
-  color: #e66456;
-  text-align: right;
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-}
-.empty {
-  text-align: center;
-  color: #666;
-  margin-top: 4em;
-}
-.empty .icon-zanwushuju {
-  font-size: 7em;
-  margin-left: 0.2em;
-  color: #fff;
-}
-.empty .text {
-  margin-top: -1.5em;
-  color: #fff;
-}
-.order-item-wrapper {
-  margin-top: 0.4em;
-  margin-bottom: 0.6em;
-  padding: 0.8em;
-  background-color: #fff;
-  border-radius: 8px;
-}
-.header {
-  height: 2em;
-  border-bottom: 1px solid #e6e6e6;
-}
-.order-num {
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #333;
-}
-.name {
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #000;
-  margin-top: 0.4em;
-  margin-bottom: 0.6em;
-}
-.count {
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #999;
-}
-.total {
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #000;
-  margin-top: 1.4em;
-}
+    .order .order-wrapper {
+        padding: 0.6em 0.8em;
+        height: calc(100% - 6.6em);
+        overflow: auto;
+        background: url("./../../assets/img/back.jpg");
+        background-size: 100%;
+        background-repeat: no-repeat;
+    }
+    
+    .order-item-wrapper {
+        margin-bottom: 1em;
+        padding: 0.5em 1em;
+        background-color: #fff;
+    }
+    
+    .primary {
+        color: #e66456;
+        text-align: right;
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+    }
+    
+    .empty {
+        text-align: center;
+        color: #666;
+        margin-top: 4em;
+    }
+    
+    .empty .icon-zanwushuju {
+        font-size: 7em;
+        margin-left: 0.2em;
+        color: #fff;
+    }
+    
+    .empty .text {
+        margin-top: -1.5em;
+        color: #fff;
+    }
+    
+    .order-item-wrapper {
+        margin-top: 0.4em;
+        margin-bottom: 0.6em;
+        padding: 0.8em;
+        background-color: #fff;
+        border-radius: 8px;
+    }
+    
+    .header {
+        height: 2em;
+        border-bottom: 1px solid #e6e6e6;
+    }
+    
+    .order-num {
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #333;
+    }
+    
+    .name {
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #000;
+        margin-top: 0.4em;
+        margin-bottom: 0.6em;
+    }
+    
+    .count {
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #999;
+    }
+    
+    .total {
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #000;
+        margin-top: 1.4em;
+    }
 </style>
--- src/views/modules/sell.vue
+++ src/views/modules/sell.vue
@@ -14,7 +14,7 @@
             <el-col :span="12" class="name">{{item.PRODUCT_NAME}}</el-col>
             <el-col :span="12" class="time ta-r">{{fmtDate(item.RELEASE_TIME)}}</el-col>
           </el-row>
-          <el-row class="order">订单号：{{item.ORDER_NO}}</el-row>
+          <el-row class="order">订单ID：{{item.PRODUCT_ID}}</el-row>
           <el-row>
             <el-col :span="12">
               <p class="order">数量：{{item.PRODUCT_NUM}}</p>
@@ -85,232 +85,264 @@
 </template>
 
 <script>
-import api from './../../api/api.js';
-import { XHeader, XDialog, XInput, XButton, Loading } from "vux";
-import scroll from './../../components/scroll';
-export default {
-  data() {
-    return {
-      goods: [],
-      pageSize: 20,
-      pageIndex: 1,
-      loadText: '',
-      showAdd: false,
-      code: "",
-      orderGoods: [],
-      tips: "",
-      loading: false,
-      showDialog: false,
+    import api from './../../api/api.js';
+    import {
+        XHeader,
+        XDialog,
+        XInput,
+        XButton,
+        Loading
+    } from "vux";
+    import scroll from './../../components/scroll';
+    export default {
+        data() {
+            return {
+                goods: [],
+                pageSize: 20,
+                pageIndex: 1,
+                loadText: '',
+                showAdd: false,
+                code: "",
+                orderGoods: [],
+                tips: "",
+                loading: false,
+                showDialog: false,
+            };
+        },
+        mounted() {
+            this.getSellProducts();
+        },
+        methods: {
+            getSellProducts(callback) {
+                let params = {
+                    showCount: this.pageSize,
+                    currentPage: this.pageIndex
+                };
+                this.loading = true;
+                api.getSellProducts(params).then(res => {
+                    this.loading = false;
+                    this.loadText = '';
+                    if (callback) callback();
+                    if (res.data.msg_code === 'success') {
+                        if (this.pageIndex === 1) {
+                            this.goods = res.data.result;
+                        } else {
+                            this.goods = this.goods.concat(res.data.result);
+                        }
+                    } else {
+                        this.$message({
+                            type: 'info',
+                            message: res.data.msg
+                        })
+                        this.goods = [];
+                    }
+                })
+            },
+            fmtDate(obj) {
+                var date = new Date(obj);
+                var y = 1900 + date.getYear();
+                var m = "0" + (date.getMonth() + 1);
+                var d = "0" + date.getDate();
+                return y + "-" + m.substring(m.length - 2, m.length) + "-" + d.substring(d.length - 2, d.length);
+            },
+            onRefresh(callback) {
+                this.getSellProducts(callback);
+            },
+            onInfinite(callback) {
+                this.loadText = '加载中';
+                this.pageIndex++;
+                this.getSellProducts(callback);
+            },
+            onCopy() {
+                this.$message({
+                    type: 'success',
+                    message: '复制成功！'
+                })
+            },
+            add() {
+                this.tips = '';
+                this.code = '';
+                this.orderGoods = [];
+                this.showAdd = true;
+            },
+            getGoods() {
+                if (this.code === '') return this.tips = '请获取商品';
+                api.release({
+                    CODE: this.code
+                }).then(res => {
+                    if (res.data.msg_code === 'success') {
+                        this.getSellProducts();
+                        this.showAdd = false;
+                    } else {
+                        this.tips = res.data.msg;
+                    }
+                });
+            },
+            getOrders() {
+                if (this.code === '') return this.tips = '请输入识别码';
+                this.orderGoods = [];
+                api.getOrders({
+                    CODE: this.code
+                }).then(res => {
+                    if (res.data && res.data.result) {
+                        this.tips = '';
+                        this.orderGoods = res.data.result;
+                    } else {
+                        this.tips = '没有商品';
+                    }
+                });
+            }
+        },
+        components: {
+            XHeader,
+            XButton,
+            XInput,
+            XDialog,
+            Loading,
+            scroll
+        }
     };
-  },
-  mounted() {
-    this.getSellProducts();
-  },
-  methods: {
-    getSellProducts(callback) {
-      let params = {
-        showCount: this.pageSize,
-        currentPage: this.pageIndex
-      };
-      this.loading = true;
-      api.getSellProducts(params).then(res => {
-        this.loading = false;
-        this.loadText = '';
-        if (callback) callback();
-        if (res.data.msg_code === 'success') {
-          if (this.pageIndex === 1) {
-            this.goods = res.data.result;
-          } else {
-            this.goods = this.goods.concat(res.data.result);
-          }
-        } else {
-          this.$message({
-            type: 'info',
-            message: res.data.msg
-          })
-          this.goods = [];
-        }
-      })
-    },
-    fmtDate(obj) {
-      var date = new Date(obj);
-      var y = 1900 + date.getYear();
-      var m = "0" + (date.getMonth() + 1);
-      var d = "0" + date.getDate();
-      return y + "-" + m.substring(m.length - 2, m.length) + "-" + d.substring(d.length - 2, d.length);
-    },
-    onRefresh(callback) {
-      this.getSellProducts(callback);
-    },
-    onInfinite(callback) {
-      this.loadText = '加载中';
-      this.pageIndex++;
-      this.getSellProducts(callback);
-    },
-    onCopy(){
-      this.$message({
-        type: 'success',
-        message: '复制成功！'
-      })
-    },
-    add() {
-      this.tips = '';
-      this.code = '';
-      this.orderGoods = [];
-      this.showAdd = true;
-    },
-    getGoods() {
-      if (this.code === '') return this.tips = '请获取商品';
-      api.release({ CODE: this.code }).then(res => {
-        if (res.data.msg_code === 'success') {
-          this.getSellProducts();
-          this.showAdd = false;
-        } else {
-          this.tips = res.data.msg;
-        }
-      });
-    },
-    getOrders() {
-      if (this.code === '') return this.tips = '请输入识别码';
-      this.orderGoods = [];
-      api.getOrders({ CODE: this.code }).then(res => {
-        if (res.data && res.data.result) {
-          this.tips = '';
-          this.orderGoods = res.data.result;
-        } else {
-          this.tips = '没有商品';
-        }
-      });
-    }
-  },
-  components: {
-    XHeader,
-    XButton,
-    XInput,
-    XDialog,
-    Loading,
-    scroll
-  }
-};
 </script>
 
 <style scoped>
-.sell .sell-wrapper {
-  padding: 0.6em 0.8em;
-  height: calc(100% - 6.6em);
-  overflow: auto;
-  background: url("./../../assets/img/back.jpg");
-  background-size: 100%;
-  background-repeat: no-repeat;
-}
-.add-btn {
-  color: #e66456;
-  float: right;
-}
-.empty {
-  text-align: center;
-  color: #666;
-  margin-top: 4em;
-}
-.empty .icon-zanwushuju {
-  font-size: 7em;
-  margin-left: 0.2em;
-  color: #fff;
-}
-.empty .text {
-  margin-top: -1.5em;
-  color: #fff;
-}
-.sell-item-wrapper {
-  position: relative;
-  margin-top: 0.4em;
-  margin-bottom: 0.6em;
-  padding: 0.8em;
-  background-color: #fff;
-  border-radius: 8px;
-}
-.name {
-  font-family: "PingFang-SC-Bold";
-  font-weight: bold;
-  font-size: 1em;
-  color: #000;
-}
-.time {
-  font-family: "PingFang-SC-Medium";
-  color: #999;
-  font-size: 1em;
-}
-.order {
-  font-family: "PingFang-SC-Medium";
-  color: #999;
-  font-size: 1em;
-}
-.money {
-  font-family: "PingFang-SC-Heavy";
-  font-size: 1em;
-  color: #e66456;
-}
-.btn {
-  margin-top: 1em;
-}
-.dialog-content {
-  padding: 1em 1.2em;
-}
-.dialog-title {
-  font-family: "PingFang-SC-Bold";
-  font-size: 1em;
-  color: #333;
-}
-.dialog-mark {
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #999;
-  margin-top: 0.125em;
-  margin-bottom: 0.8em;
-}
-.dialog-content .weui-cell {
-  border-radius: 6px;
-  background-color: #f5f5f5;
-  border: 1px solid #e5e5e5;
-  padding: 0.5em 1em;
-}
-.dialog-content .weui-cell:before {
-  border-top: 0 !important;
-}
-.dialog-content .remarks {
-  margin-top: 1em;
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-  color: #808080;
-  text-align: left;
-}
-.dialog-footer {
-  border-top: 1px solid #ccc;
-  height: 3.6em;
-  line-height: 3.6em;
-  font-family: "PingFang-SC-Medium";
-  font-size: 1em;
-}
-.dialog-remarks{
-  font-family: 'PingFang-SC-Medium';
-  font-size: 0.6em;
-  color: #666;
-  /* margin-top: -0.5em; */
-}
-.dialog-qq{
-  font-family: 'PingFang-SC-Medium';
-  font-size: 1em;
-  color: #333;
-  padding-top: 0.5em;
-}
-.order-goods {
-  padding: 1em 0;
-}
-.tips {
-  margin-top: 1em;
-  color: red;
-}
-.primary {
-  color: #e66456;
-}
+    .sell .sell-wrapper {
+        padding: 0.6em 0.8em;
+        height: calc(100% - 6.6em);
+        overflow: auto;
+        background: url("./../../assets/img/back.jpg");
+        background-size: 100%;
+        background-repeat: no-repeat;
+    }
+    
+    .add-btn {
+        color: #e66456;
+        float: right;
+    }
+    
+    .empty {
+        text-align: center;
+        color: #666;
+        margin-top: 4em;
+    }
+    
+    .empty .icon-zanwushuju {
+        font-size: 7em;
+        margin-left: 0.2em;
+        color: #fff;
+    }
+    
+    .empty .text {
+        margin-top: -1.5em;
+        color: #fff;
+    }
+    
+    .sell-item-wrapper {
+        position: relative;
+        margin-top: 0.4em;
+        margin-bottom: 0.6em;
+        padding: 0.8em;
+        background-color: #fff;
+        border-radius: 8px;
+    }
+    
+    .name {
+        font-family: "PingFang-SC-Bold";
+        font-weight: bold;
+        font-size: 1em;
+        color: #000;
+    }
+    
+    .time {
+        font-family: "PingFang-SC-Medium";
+        color: #999;
+        font-size: 1em;
+    }
+    
+    .order {
+        font-family: "PingFang-SC-Medium";
+        color: #999;
+        font-size: 1em;
+    }
+    
+    .money {
+        font-family: "PingFang-SC-Heavy";
+        font-size: 1em;
+        color: #e66456;
+    }
+    
+    .btn {
+        margin-top: 1em;
+    }
+    
+    .dialog-content {
+        padding: 1em 1.2em;
+    }
+    
+    .dialog-title {
+        font-family: "PingFang-SC-Bold";
+        font-size: 1em;
+        color: #333;
+    }
+    
+    .dialog-mark {
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #999;
+        margin-top: 0.125em;
+        margin-bottom: 0.8em;
+    }
+    
+    .dialog-content .weui-cell {
+        border-radius: 6px;
+        background-color: #f5f5f5;
+        border: 1px solid #e5e5e5;
+        padding: 0.5em 1em;
+    }
+    
+    .dialog-content .weui-cell:before {
+        border-top: 0 !important;
+    }
+    
+    .dialog-content .remarks {
+        margin-top: 1em;
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+        color: #808080;
+        text-align: left;
+    }
+    
+    .dialog-footer {
+        border-top: 1px solid #ccc;
+        height: 3.6em;
+        line-height: 3.6em;
+        font-family: "PingFang-SC-Medium";
+        font-size: 1em;
+    }
+    
+    .dialog-remarks {
+        font-family: 'PingFang-SC-Medium';
+        font-size: 0.6em;
+        color: #666;
+        /* margin-top: -0.5em; */
+    }
+    
+    .dialog-qq {
+        font-family: 'PingFang-SC-Medium';
+        font-size: 1em;
+        color: #333;
+        padding-top: 0.5em;
+    }
+    
+    .order-goods {
+        padding: 1em 0;
+    }
+    
+    .tips {
+        margin-top: 1em;
+        color: red;
+    }
+    
+    .primary {
+        color: #e66456;
+    }
 </style>
